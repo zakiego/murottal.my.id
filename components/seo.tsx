9a82dbb08e01382c95b12355bc3ea566aaf7c638
Zakiego <belummikir@gmail.com>
2022-04-03 14:33:54 +0800

    refactor: add next-seo

---
+++ components/seo.tsx
@@ -0,0 +1,31 @@
+import { NextSeo } from "next-seo";
+
+export default function SEO() {
+  const meta = {
+    title: "Kumpulan Murottal",
+    description: "Temukan qari kesukaanmu",
+    creator: "@zakiego",
+    url: "https://murottal.my.id",
+    image:
+      "https://hqxduecuxrozqlaicqkg.supabase.in/storage/v1/object/public/qari/banner.png",
+  };
+  return (
+    <NextSeo
+      title={meta.title}
+      description={meta.description}
+      canonical={meta.url}
+      openGraph={{
+        url: meta.url,
+        title: meta.title,
+        description: meta.description,
+        images: [{ url: meta.image }],
+        site_name: meta.title,
+      }}
+      twitter={{
+        handle: "@handle",
+        site: "@site",
+        cardType: "summary_large_image",
+      }}
+    />
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import Head from "next/head";
-
 import { Navbar, QariCard, Saran } from "~/components";
+import SEO from "~/components/seo";
 import { QariType } from "~/lib/qari";
 
 export async function getStaticProps() {
-  const data = await fetch("https://murottal.my.id/api/v1/qari").then((resp) =>
-    resp.json(),
-  );
+  const url = process.env.NEXT_URL || process.env.VERCEL_URL;
+  const data = await fetch(`${url}/api/v1/qari`).then((resp) => resp.json());
 
   if (!data) {
     return {
@@ -20,35 +18,9 @@ export async function getStaticProps() {
 }
 
 export default function Index({ data }: { data: QariType[] }) {
-  const meta = {
-    title: "Kumpulan Murottal",
-    description: "Temukan qari kesukaanmu",
-    creator: "@zakiego",
-    url: "murottal.my.id",
-    image:
-      "https://hqxduecuxrozqlaicqkg.supabase.in/storage/v1/object/public/qari/banner.png",
-  };
   return (
     <div className="min-h-screen px-6 md:px-11 lg:px-16 py-7 md:py-14 from-white via-white to-[#EBF0FE] bg-gradient-to-br">
-      <Head>
-        <title>{meta.title}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
-        <meta name="description" content={meta.description} />
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" key="twcard" />
-        <meta name="twitter:creator" content={meta.creator} key="twhandle" />
-        {/* Open Graph */}
-        <meta property="og:url" content={meta.url} key="ogurl" />
-        <meta property="og:image" content={meta.image} key="ogimage" />
-        <meta property="og:site_name" content={meta.url} key="ogsitename" />
-        <meta property="og:title" content={meta.title} key="ogtitle" />
-        <meta
-          property="og:description"
-          content={meta.description}
-          key="ogdesc"
-        />
-      </Head>
+      <SEO />
       <body>
         <nav>
           <Navbar />
